0609252974f9f05cdb7ba391888d144b0def5161
Rodrigo Igor Villanueva Nieto <rodrigo.villanueva.c@senasica.gob.mx>
2019-01-22 12:30:27 -0600

    Roles y factories

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Caffeinated\Shinobi\Models\Role; //agragar rol
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +13,21 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
          $this->call(PermissionsTableSeeder::class);
+         factory(App\Product::class, 50)->create();
+         factory(App\User::class, 2)->create();
+
+
+         Role::create([
+            'name'  => 'Admin',
+            'slug'  => 'admin',
+            'special' => 'all-access'
+        ]);
+
+        App\User::create([
+            'name'=>'rodrigo',
+            'email'=>'rodrigo@gmail.com',
+            'password'=>bcrypt('rorro'),
+
+        ]);
     }
 }
